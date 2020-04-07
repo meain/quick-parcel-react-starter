9a5546d1b342c38ad17bd006fb89ef2bcb7a15ac
Abin Simon <abinsimon10@gmail.com>
2020-04-07 15:21:00 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,6 @@ module.exports = {
     "no-use-before-define": 0,
     indent: ["error", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
     "no-use-before-define": 0
   }
 };
